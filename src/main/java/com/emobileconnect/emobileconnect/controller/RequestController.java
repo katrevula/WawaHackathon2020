f9d4fd0dfe27c3911dbfcd7719c374115648cb45
KUNDURTHI <sathi.0010@gmail.com>
2020-11-08 13:51:59 -0600

    Request Controller change added code

--- src/main/java/com/emobileconnect/emobileconnect/controller/RequestController.java
+++ src/main/java/com/emobileconnect/emobileconnect/controller/RequestController.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emobileconnect.emobileconnect.dto.RequestDTO;
+import com.emobileconnect.emobileconnect.model.Request;
 import com.emobileconnect.emobileconnect.service.impl.RequestService;
 
 @RestController
@@ -32,5 +34,16 @@ public class RequestController {
 
 	    
 	}
-
+	
+	@GetMapping("/tracking")
+	public ResponseEntity<?> getRequestTracking(Request request){
+		try {
+			
+			return  ResponseEntity.ok().body(requestService.getRequestTracking(request));
+			
+		}catch(Exception e){
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
 }
