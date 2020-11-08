0920bd93bc8ad78018655f34d04ba85074d741a3
katrevula <Sandeep.katrevula@gmail.com>
2020-11-08 10:37:20 -0600

    Plan Entity, Service implementation structure updated

--- src/main/java/com/emobileconnect/emobileconnect/model/Plan.java
+++ src/main/java/com/emobileconnect/emobileconnect/model/Plan.java
@@ -1,4 +1,25 @@
 package com.emobileconnect.emobileconnect.model;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name = "plan")
+@SequenceGenerator(name = "seq", initialValue = 1)
 public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
+    private int plan_id;
+
+    @NotNull
+    private String plan_Name;
+
+    @NotNull
+    private int Talktime;
+
+    @NotNull
+    private double Data;
 }
--- src/main/java/com/emobileconnect/emobileconnect/service/impl/PlanService.java
+++ src/main/java/com/emobileconnect/emobileconnect/service/impl/PlanService.java
@@ -1,4 +1,29 @@
 package com.emobileconnect.emobileconnect.service.impl;
 
-public class PlanService {
+import com.emobileconnect.emobileconnect.model.Plan;
+
+import java.util.List;
+
+public interface PlanService  {
+
+    List<Plan> getAllplans();
+
+    boolean isPlanExist(Plan plan);
+
+    boolean isLetter(Plan plan);
+
+    Plan addPlan(Plan plan);
+
+    Plan getByID(int id);
+
+    Plan editPlan(Plan plan);
+
+    Plan getByName(char name);
+
+    void updatePlan(Plan plan);
+
+    void deletePlan(int id);
+
+
+
 }
