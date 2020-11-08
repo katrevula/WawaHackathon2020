9ca1fd689466fc68dd03a733ee13cfebe06c295a
katrevula <Sandeep.katrevula@gmail.com>
2020-11-08 12:43:54 -0600

    Delete method with to delete plans

--- src/main/java/com/emobileconnect/emobileconnect/controller/PlanController.java
+++ src/main/java/com/emobileconnect/emobileconnect/controller/PlanController.java
@@ -9,11 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 import java.util.List;
+
 @RestController
 public class PlanController {
 
@@ -39,21 +42,37 @@ public class PlanController {
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
-// Get all Plans
-    public ResponseEntity<List<Plan>> listAllPlans(){
+    // Get all Plans
+    @RequestMapping(value = "/plans", method = RequestMethod.GET)
+    public ResponseEntity<List<Plan>> listAllPlans() {
         List<Plan> plans = planService.getAllplans();
 
-        if(plans.isEmpty()){
+        if (plans.isEmpty()) {
             return new ResponseEntity(HttpStatus.NO_CONTENT);
-        }
-        else{
-            return new ResponseEntity<List<Plan>>(plans,HttpStatus.OK);
+        } else {
+            return new ResponseEntity<List<Plan>>(plans, HttpStatus.OK);
         }
     }
 
+    // Updating all plans
+    @RequestMapping(value = "/Deleteplan", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deletePlan(@RequestBody Plan plan, UriComponentsBuilder ubuilder) {
+        Plan currentPlan = planService.getByID(plan.getPlan_id());
+        logger.info(String.format("Plan name %s", plan.getPlan_Name()));
 
+        if(currentPlan == null){
+            logger.error("Unable to Delete. Plan with the following name {} does not exist", plan.getPlan_Name());
+            return new ResponseEntity(new CustomErrorType("Unable to delete.  The Plan with following name does not exist: " + plan.getPlan_Name()),
+                    HttpStatus.CONFLICT);
+        }
 
+        planService.deletePlan(plan.getPlan_id());
+        logger.info("Printing plan value" + plan.getPlan_Name());
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ubuilder.path("/api/Deleteplan/").buildAndExpand(plan.getPlan_id()).toUri());
+        return new ResponseEntity<>(headers, HttpStatus.OK);
+    }
 
 
 }
