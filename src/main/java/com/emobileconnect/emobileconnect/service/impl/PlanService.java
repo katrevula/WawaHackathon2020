2f6d4624e1fbb5e1bdd110ab0c4af1439ea1a021
katrevula <Sandeep.katrevula@gmail.com>
2020-11-08 10:41:03 -0600

    Plan Service Implementaion methods Updated

--- src/main/java/com/emobileconnect/emobileconnect/service/impl/PlanService.java
+++ src/main/java/com/emobileconnect/emobileconnect/service/impl/PlanService.java
@@ -18,7 +18,7 @@ public interface PlanService  {
 
     Plan editPlan(Plan plan);
 
-    Plan getByName(char name);
+    Plan getByName(String name);
 
     void updatePlan(Plan plan);
 
--- src/main/java/com/emobileconnect/emobileconnect/service/impl/PlanServiceImpl.java
+++ src/main/java/com/emobileconnect/emobileconnect/service/impl/PlanServiceImpl.java
@@ -1,4 +1,52 @@
 package com.emobileconnect.emobileconnect.service.impl;
 
-public class PlanServiceImpl {
+import com.emobileconnect.emobileconnect.model.Plan;
+
+import java.util.List;
+
+public class PlanServiceImpl implements PlanService {
+    @Override
+    public List<Plan> getAllplans() {
+        return null;
+    }
+
+    @Override
+    public boolean isPlanExist(Plan plan) {
+        return false;
+    }
+
+    @Override
+    public boolean isLetter(Plan plan) {
+        return false;
+    }
+
+    @Override
+    public Plan addPlan(Plan plan) {
+        return null;
+    }
+
+    @Override
+    public Plan getByID(int id) {
+        return null;
+    }
+
+    @Override
+    public Plan editPlan(Plan plan) {
+        return null;
+    }
+
+    @Override
+    public Plan getByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void updatePlan(Plan plan) {
+
+    }
+
+    @Override
+    public void deletePlan(int id) {
+
+    }
 }
