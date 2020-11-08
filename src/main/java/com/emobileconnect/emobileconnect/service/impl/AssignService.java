c490403062d2b81a2d7d03dd6a52112d3db32f80
katrevula <Sandeep.katrevula@gmail.com>
2020-11-08 12:22:20 -0600

    Listing all plans updated

---
+++ src/main/java/com/emobileconnect/emobileconnect/service/impl/AssignService.java
@@ -0,0 +1,58 @@
+package com.emobileconnect.emobileconnect.service.impl;
+
+<<<<<<< HEAD:src/main/java/com/emobileconnect/emobileconnect/service/impl/PlanServiceImpl.java
+import com.emobileconnect.emobileconnect.dao.PlanRepository;
+import com.emobileconnect.emobileconnect.model.Plan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlanServiceImpl implements PlanService {
+
+
+    @Autowired
+    private PlanRepository planRepository;
+
+
+    @Override
+    public List<Plan> getAllplans() {
+        return planRepository.findAll();
+    }
+
+    @Override
+    public boolean isPlanExist(Plan plan) {
+        return plan.equals(getAllplans());
+    }
+
+    @Override
+    public Plan addPlan(Plan plan) {
+        Plan savedPlan = planRepository.saveAndFlush(plan);
+        return plan;
+    }
+
+
+    @Override
+    public Plan getByID(int id) {
+        return planRepository.findByPlanId(id);
+    }
+
+    @Override
+    public Plan editPlan(Plan plan) {
+        return planRepository.saveAndFlush(plan);
+    }
+
+    @Override
+    public void updatePlan(Plan plan) {
+        planRepository.saveAndFlush(plan);
+    }
+
+    @Override
+    public void deletePlan(int id) {
+        planRepository.deleteById(id);
+    }
+=======
+public interface AssignService {
+>>>>>>> origin/feature/assginePhonetoCustomer:src/main/java/com/emobileconnect/emobileconnect/service/impl/AssignService.java
+}
